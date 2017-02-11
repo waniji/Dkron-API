4b11c56a5ec86b79ccdf5e33cdd1d6fbea0eef78
Makoto Sasaki <wanigoya@gmail.com>
2017-02-11 16:05:08 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Dkron::API - It's new $module
+
+# SYNOPSIS
+
+    use Dkron::API;
+
+# DESCRIPTION
+
+Dkron::API is ...
+
+# LICENSE
+
+Copyright (C) Makoto Sasaki.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Makoto Sasaki <>
---
+++ lib/Dkron/API.pm
@@ -0,0 +1,39 @@
+package Dkron::API;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Dkron::API - It's new $module
+
+=head1 SYNOPSIS
+
+    use Dkron::API;
+
+=head1 DESCRIPTION
+
+Dkron::API is ...
+
+=head1 LICENSE
+
+Copyright (C) Makoto Sasaki.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Makoto Sasaki E<lt>E<gt>
+
+=cut
+
